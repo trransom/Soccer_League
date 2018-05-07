9804463801a981553ab22d4c1048e3e4d30aa13a
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-07 11:04:42 -0400

    Turned teams into libraries, created logic to separate experienced from nonexperienced players

--- league_builder.py
+++ league_builder.py
@@ -1,10 +1,25 @@
 # League Builder
 import csv
-Sharks = []
-Raptors = []
-Dragons = []
+
+Sharks = {}
+Raptors = {}
+Dragons = {}
+
+experienced = []
+not_experienced = []
 
 if __name__ == "__main__":
-	with open(soccer_players.csv, newline=' ') as soccerfile:
-	sccr_reader = csv.reader(soccer_players.csv, delimeter = ',')
-	rows = list(reader)
+	with open('soccer_players.csv', newline='') as soccerfile:
+		sccr_reader = csv.reader(soccerfile, delimiter = ',')
+		rows = list(sccr_reader)
+		for row in rows[1:-1]:
+			if row[2]=='YES':
+				experienced.append(row)
+			else:
+				not_experienced.append(row)
+		print("experienced: ")
+		for name in experienced:
+			print(name)
+		print("not experienced: ")
+		for name in not_experienced:
+			print(name)
