ec8bd1f969814f388cd9126ff859efccd6f5b9ae
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-07 10:08:18 -0400

    Deleted 'leauge_builder'

--- league_builder.py
+++ league_builder.py
@@ -5,3 +5,6 @@ Raptors = []
 Dragons = []
 
 if __name__ == "__main__":
+	with open(soccer_players.csv, newline=' ') as soccerfile:
+	sccr_reader = csv.reader(soccer_players.csv, delimeter = ',')
+	rows = list(reader)
